2ce77d3c47ecf37277bae75278b4ac8042fe1256
Reuben <stupidpower95@gmail.com>
2023-12-09 23:52:23 +0000

    job_script_generator done

---
+++ jobscript_Birmingham.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#======================================================
+#
+# Job script for running GROMACS on multiple cores on a single node
+# Adapted to activate momepy conda environment and run Jupyter notebook
+#
+#======================================================
+
+#======================================================
+# Propagate environment variables to the compute node
+#SBATCH --export=ALL
+#
+# Run in the standard partition (queue)
+#SBATCH --partition=standard
+#
+# Specify project account
+#SBATCH --account=porta-umm
+#
+# No. of tasks required (max of 40), all cores on the same node
+#SBATCH --ntasks=1 --cpus-per-task=16
+#
+# Specify (hard) runtime (HH:MM:SS)
+#SBATCH --time=08:00:00
+#
+# Job name
+#SBATCH --job-name=Birmingham_morph
+#
+# Output file
+#SBATCH --output=slurm-%j.out
+#======================================================
+
+module purge
+module load miniconda/3.11.4
+
+# Activate the momepy conda environment
+conda activate momepy
+
+# Set the number of threads
+export OMP_NUM_THREADS=16
+
+#======================================================
+# Prologue script to record job details
+# Do not change the line below
+#======================================================
+/opt/software/scripts/job_prologue.sh  
+#------------------------------------------------------
+
+# Run the Jupyter notebook
+
+papermill 3_supercomp_morph.ipynb output/Birmingham_3_supercomp_morph.ipynb -p local_crs 3414 -p place Birmingham -p lat 52.47790683186595 -p lng -1.8999014571831943 -p crs 4326 -p local_crs 27700
+
+#======================================================
+# Epilogue script to record job endtime and runtime
+# Do not change the line below
+#======================================================
+/opt/software/scripts/job_epilogue.sh 
+#------------------------------------------------------
