6de32c80a90693a38890315a212183e545e034dd
virgilxw <reubenwxw@gmail.com>
2024-02-16 16:42:56 +0800

    everything works

---
+++ jobscript_Bristol.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#======================================================
+#
+# Job script for running GROMACS on multiple cores on a single node
+# Adapted to activate momepy conda environment and run Jupyter notebook
+#
+#======================================================
+
+#======================================================
+# Propagate environment variables to the compute node
+#SBATCH --export=ALL
+#
+# Run in the standard partition (queue)
+#SBATCH --partition=standard
+#
+# Specify project account
+#SBATCH --account=porta-umm
+#
+# No. of tasks required (max of 40), all cores on the same node
+#SBATCH --ntasks=1 --cpus-per-task=16
+#
+# Specify (hard) runtime (HH:MM:SS)
+#SBATCH --time=08:00:00
+#
+# Job name
+#SBATCH --job-name=Bristol_morph
+#
+# Output file
+#SBATCH --output=slurm-%j.out
+#======================================================
+
+module purge
+module load miniconda/3.11.4
+
+# Activate the momepy conda environment
+conda activate momepy
+
+# Set the number of threads
+export OMP_NUM_THREADS=16
+
+#======================================================
+# Prologue script to record job details
+# Do not change the line below
+#======================================================
+/opt/software/scripts/job_prologue.sh  
+#------------------------------------------------------
+
+# Run the Jupyter notebook
+
+papermill 4_aggregation.ipynb output/Bristol_4_aggregation.ipynb -p local_crs 4326 -p place Bristol -p lat 51.453611 -p lng -2.5975 -p country {country} -p crs 4326
+
+#======================================================
+# Epilogue script to record job endtime and runtime
+# Do not change the line below
+#======================================================
+/opt/software/scripts/job_epilogue.sh 
+#------------------------------------------------------
